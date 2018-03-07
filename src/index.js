44c51c0cf04f7f57b5a80e27ba0f465ee1237078
godofnoobs <n_ska@bk.ru>
2018-03-07 15:40:23 +0300

    upgrade algo to do less repititions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-module.exports = function longestConsecutiveLength(array) {
-//function longestConsecutiveLength(array) {
-    var arr = array.slice();
+module.exports = function longestConsecutiveLength(arr) {
     var count = 0;
     var max = 0;
     var o = {};
@@ -9,17 +7,17 @@ module.exports = function longestConsecutiveLength(array) {
     if (!arr) return 0;
     for (var i = 0; i < arr.length; i++)
         o[arr[i]] = 1;
-    
-
     for (var i = 0; i < arr.length; i++) {
         count = 0;
+        var c = 0;
         current = arr[i];
         while (o[current++]) {
+            c++;
             count++;
         }
+        i += c-1;
         if (count > max)
             max = count;
     }
-    
     return max;
 };
